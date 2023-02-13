456a383f8fdacc5e9a1aeac7f4a129fb45eed2ac
johnsun <jsu@discoverelement.com>
2023-02-12 19:33:41 -0800

    cuda test

--- cuda_troch_test.py
+++ cuda_troch_test.py
@@ -5,12 +5,11 @@ import numpy as np
 
 print(torch.cuda.is_available())
 
-device = torch.device("cpu")
-image = np.random.random(size=[2, 3, 224, 224])
-image.dtype = 'float32'
-
-image_tensor = torch.from_numpy(image).to(device)
-
+device = torch.device("cuda")
+image = np.zeros((2,3,224, 224))
+print( image.shape )
+image_tensor = torch.from_numpy(image).type(torch.float).to(device)
+print( image_tensor.shape )
 model = models.resnet50(pretrained=True)
 model = model.to(device)
 
